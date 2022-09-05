3a6676e0d16fbcf10fa097e773871aacd03bba7f
timothy adigun <adigun.timothy@gmail.com>
2022-09-06 00:22:57 +0100

    modified the count-down file

--- ch04/count-down.ex
+++ ch04/count-down.ex
@@ -3,18 +3,12 @@ defmodule Count do
 
   def count_down(count) when count >= 0 do
     IO.inspect(count)
-    count_down(count)
-  end
-
-  @spec count_down(number()) :: number()
-
-  defp count_down(count) do
     count_down(count - 1)
   end
 
-  @spec count_down(number()) :: number()
+  @spec count_down(number()) :: :ok
 
   def count_down(_count) do
-      IO.puts("finished!")
+    IO.puts("finished!")
   end
 end
