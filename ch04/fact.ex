4829d7db81838e236cba5597b3cb5737e9dc53f3
timothy adigun <adigun.timothy@gmail.com>
2022-09-07 00:09:11 +0100

    modified fact file again

--- ch04/fact.ex
+++ ch04/fact.ex
@@ -8,14 +8,17 @@ defmodule Fact do
   end
 
   def fact!(num) do
+    IO.inspect("#{num}")
     fact!(num, 1)
   end
 
   defp fact!(num, acc) when num == 1 do
+    IO.inspect(acc)
     acc
   end
 
   defp fact!(num, acc) do
+    IO.inspect("#{num} - #{acc}")
     fact!(num - 1, acc * num)
   end
 end
