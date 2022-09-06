e55418f8ca23f5b2ef176869e4e04b8455baadc1
timothy adigun <adigun.timothy@gmail.com>
2022-09-07 00:00:15 +0100

    modified fact file

--- ch04/fact.ex
+++ ch04/fact.ex
@@ -8,12 +8,14 @@ defmodule Fact do
   end
 
   def fact!(num) do
-      fact!(num, 1)
+    fact!(num, 1)
   end
 
-  defp fact!(num, acc), when num == 1 do: acc
+  defp fact!(num, acc) when num == 1 do
+    acc
+  end
+
   defp fact!(num, acc) do
     fact!(num - 1, acc * num)
   end
-
 end
