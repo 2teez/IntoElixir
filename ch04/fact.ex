5e34d05bd053fb8f00080fa0f770739d12942ecb
timothy adigun <adigun.timothy@gmail.com>
2022-09-06 23:51:26 +0100

    added tail recursion to factorial file

--- ch04/fact.ex
+++ ch04/fact.ex
@@ -6,4 +6,14 @@ defmodule Fact do
       num * factorial(num - 1)
     end
   end
+
+  def fact!(num) do
+      fact!(num, 1)
+  end
+
+  defp fact!(num, acc), when num == 1 do: acc
+  defp fact!(num, acc) do
+    fact!(num - 1, acc * num)
+  end
+
 end
