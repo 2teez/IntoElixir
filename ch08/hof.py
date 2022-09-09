a7b373860831d995b5600ccd9753d283d49f1194
timothy adigun <adigun.timothy@gmail.com>
2022-09-09 08:49:38 +0100

    modified an hof py file

--- ch08/hof.py
+++ ch08/hof.py
@@ -20,6 +20,12 @@ def _print_menum(e: MEnum) -> None:
 def main():
     ec = MEnum([1, 2, 3, 4])
     _print_menum(ec)
+    # using anonymous function called :- lambda
+    print(list(map(lambda x: x*x, list(range(1, 16)))))
+    fn = (lambda x: x * x)
+    print(list(map(fn, list(range(1, 16)))))
+    # using list comphrension
+    print(list(x*x for x in range(1, 16)))
 
 
 if __name__ == '__main__':
