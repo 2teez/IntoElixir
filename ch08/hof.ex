547ac95ca452acfeb5de0a0c87ba83c146f14c6b
timothy adigun <adigun.timothy@gmail.com>
2022-09-09 07:29:53 +0100

    add main function

--- ch08/hof.ex
+++ ch08/hof.ex
@@ -3,3 +3,14 @@ defmodule HOF do
     3 * function.(value)
   end
 end
+
+defmodule Main do
+  def main() do
+    my_function = fn value -> 20 * value end
+    IO.puts(to_string("#{HOF.tripple(3, my_function)}"))
+
+    # use closure
+    x = 6
+    IO.puts(to_string("#{HOF.tripple(x, my_function)}"))
+  end
+end
