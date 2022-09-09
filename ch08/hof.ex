0c98a3ea6141004ddc05251023c7133b205f585d
timothy adigun <adigun.timothy@gmail.com>
2022-09-09 07:32:45 +0100

    used another anonymous function style

--- ch08/hof.ex
+++ ch08/hof.ex
@@ -1,4 +1,4 @@
-defmodule HOF do
+defmodule Hof do
   def tripple(value, function) do
     3 * function.(value)
   end
@@ -7,10 +7,13 @@ end
 defmodule Main do
   def main() do
     my_function = fn value -> 20 * value end
-    IO.puts(to_string("#{HOF.tripple(3, my_function)}"))
+    IO.puts(to_string("#{Hof.tripple(3, my_function)}"))
 
     # use closure
     x = 6
-    IO.puts(to_string("#{HOF.tripple(x, my_function)}"))
+    IO.puts(to_string("#{Hof.tripple(x, my_function)}"))
+
+    # using another anonymous function style
+    IO.puts(to_string("#{Hof.tripple(3, &(200, &1))}"))
   end
 end
